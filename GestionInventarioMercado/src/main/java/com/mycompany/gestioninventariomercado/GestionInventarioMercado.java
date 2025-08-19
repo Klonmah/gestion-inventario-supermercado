e5e204d89e4b5bcdcf3b5a74b5ffb9cc7416bfb4
Your Name <you@example.com>
2025-08-18 20:48:58 -0400

    producto

--- GestionInventarioMercado/src/main/java/com/mycompany/gestioninventariomercado/GestionInventarioMercado.java
+++ GestionInventarioMercado/src/main/java/com/mycompany/gestioninventariomercado/GestionInventarioMercado.java
@@ -10,3 +10,12 @@ public class GestionInventarioMercado {
         System.out.println("Hello World!");
     }
 }
+
+class Producto{
+    private String[]nombresProducto;
+    private double[]preciosProducto;
+    public Producto(int tamaño){
+        nombresProducto = new String[tamaño];
+        preciosProducto = new double[tamaño];
+    }
+}
